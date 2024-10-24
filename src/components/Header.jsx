fe4ba4773c9eb22501b807f2e6d078653a27c120
Pedro Visconti Guidotte <peguidotte@gmail.com>
2024-10-24 18:59:46 -0300

    Inicio Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,77 @@
-// import { useState } from "react";
+import "../index.css";
 import { NavLink } from "react-router-dom";
 import BotaoLogin from "./BotaoLogin";
 import { useState } from "react";
+import TvIcon from "@mui/icons-material/Tv";
+import PublicIcon from "@mui/icons-material/Public";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from '@mui/icons-material/Search';
+import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+import TheaterComedyOutlinedIcon from '@mui/icons-material/TheaterComedyOutlined';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import EmailIcon from '@mui/icons-material/Email';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import SettingsIcon from '@mui/icons-material/Settings';
 
-export default function Header(){
+export default function Header() {
+  const [isLogged, setIsLogged] = useState(false);
 
-    const [isLogged, setIsLogged] = useState(false);
+  const handleLogin = () => {
+    setIsLogged(!isLogged);
+  };
 
-    const handleLogin = () => {
-        setIsLogged(!isLogged);
-    }
-    
-    return(
-        <>
-            <header className="w-full h-10 flex justify-between items-center p-10 mb-10 bg-purple-900">
-                <div>
-                    <h1>Portal Filmes</h1>
-                </div>
-                <nav>
-                    <ul className="flex gap-5">
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/movies">Pesquisar</NavLink></li>
-                        <li><NavLink to="/genre">Gêneros</NavLink></li>
-                        <li><NavLink to="/contato">Contato</NavLink></li>
-                        {isLogged && <li><NavLink to="/settings">Configurações</NavLink></li>}
-                    </ul>
-                </nav>
-                <BotaoLogin isLogged={isLogged} handleLogin={handleLogin}/>
-            </header>
-        </>
-    )
+const itensNav = [
+    { name: "Home", to: "/", activeIcon: <HomeIcon />, inactiveIcon: <HomeOutlinedIcon /> },
+    { name: "Pesquisar", to: "/movies", activeIcon: <SearchIcon/>, inactiveIcon: <SavedSearchIcon/> },
+    { name: "Gêneros", to: "/genre", activeIcon: <TheaterComedyIcon/>, inactiveIcon: <TheaterComedyOutlinedIcon/> },
+    { name: "Contato", to: "/contato", activeIcon: <EmailIcon/>, inactiveIcon: <EmailOutlinedIcon/> },
+    { name: "Configurações", to: "/settings", activeIcon: <SettingsIcon/>, inactiveIcon: <SettingsOutlinedIcon/>, requiresAuth: true }
+];
+
+  return (
+    <>
+      <header className="w-full h-10 flex justify-between items-center p-10 mb-10 bg-purple-900">
+        <div className="flex items-center gap-2 relative">
+          <TvIcon
+            sx={{ fontSize: 60 }}
+            className="absolute -right-12 opacity-50 z-0"
+          />
+          <h1 className="text-4xl movieworld">MovieWorld</h1>
+          <PublicIcon
+            fontSize="large"
+            className="absolute -right-9 -top-[0.5px] opacity-50 z-0"
+          />
+        </div>
+        <nav className="flex gap-10 items-center">
+          <ul className="flex gap-8">
+            <NavLink to="/" className={"text-xl font-medium relative"} exact>
+              {({ isActive }) => (
+                <>
+                  <p className="absolute -left-4 opacity-50 z-0">{isActive ? <HomeIcon /> : <HomeOutlinedIcon />}</p>
+                  <p>Home</p>
+                </>
+              )}
+            </NavLink>
+            <li>
+              <NavLink to="/movies">Pesquisar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/genre">Gêneros</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contato">Contato</NavLink>
+            </li>
+            {isLogged && (
+              <li>
+                <NavLink to="/settings">Configurações</NavLink>
+              </li>
+            )}
+          </ul>
+          <BotaoLogin isLogged={isLogged} handleLogin={handleLogin} />
+        </nav>
+      </header>
+    </>
+  );
 }
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,100 +1,114 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./MovieCard.css";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import Tooltip from "@mui/material/Tooltip";
-import Fade from "@mui/material/Fade";
+import {
+  Favorite as FavoriteIcon,
+  FavoriteBorder as FavoriteBorderIcon,
+  AddCircleOutline as AddCircleOutlineIcon,
+  AddCircle as AddCircleIcon,
+} from "@mui/icons-material";
+import { Tooltip, Fade } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { tooltipClasses } from "@mui/material/Tooltip";
 
-export default function MovieCard({ id, title, poster_path }) {
+// Customização da Tooltip
+const CustomTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "0.7rem",
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "black",
+  },
+});
 
+export default function MovieCard({ id, title, poster_path }) {
   const [isFavorite, setIsFavorite] = useState(false);
-  const [isAdd, setIsAdd] = useState(false)
+  const [isAdd, setIsAdd] = useState(false);
 
   const toggleAdd = () => {
-    setIsAdd(!isAdd)
-  }
+    setIsAdd(!isAdd);
+  };
 
   const toggleFavorite = () => {
     setIsFavorite(!isFavorite);
   };
 
-  const CustomTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: "black",
-      color: "white",
-      fontSize: "0.7rem",
-    },
-    [`& .${tooltipClasses.arrow}`]: {
-      color: "black",
-    },
-  });
-
   return (
-    <>
-      <div
-        key={id}
-        className="movie-card flex flex-col text-left justify-center items-center hover:scale-125 focus-within:scale-125 duration-300"
-      >
-        <Link to={`/movies/${id}`}>
-          <img
-            src={`https://image.tmdb.org/t/p/w185${poster_path}`}
-            alt={title}
-            className="movie-img h-72 rounded-sm"
-          />
-          <div className="movie-info top-[0] p-2">
-            <h2 className="text-base">{title}</h2>
-            <p className="text-xs text-gray-600">clique para ver detalhes</p>
-          </div>
-        </Link>
-        <div className="movie-info bottom-[2%] px-2">
-          <CustomTooltip
-            placement="top"
-            arrow
-            TransitionComponent={Fade}
-            TransitionProps={{ timeout: 300 }}
-            title={
-              isFavorite ? "Remover dos Favoritos" : "Adicionar aos Favoritos"
-            }
-          >
-            <button
-              onClick={toggleFavorite}
-              className="pr-1"
-            >
-              {isFavorite ? (
-                <FavoriteIcon className="icon text-[red]" />
-              ) : (
-                <FavoriteBorderIcon className="icon text-white" />
-              )}
-            </button>
-          </CustomTooltip>
-          <CustomTooltip
-            placement="top"
-            arrow
-            TransitionComponent={Fade}
-            TransitionProps={{ timeout: 300 }}
-            title={
-              isAdd ? "Remover da Lista de interesse" : "Adicionar a Lista de interesse"
-            }
-          >
-            <button
-              onClick={toggleAdd}
-            >
-              {isAdd ? (
-                <AddCircleIcon className="icon" />
-              ) : (
-                <AddCircleOutlineIcon className="icon" />
-              )}
-            </button>
-          </CustomTooltip>
+    <div
+      key={id}
+      className="movie-card flex flex-col text-left justify-center items-center hover:scale-125 focus-within:scale-125 duration-300"
+      aria-labelledby={`movie-title-${id}`} // Associate the card with the title
+    >
+      <Link to={`/movies/${id}`}>
+        <img
+          src={`https://image.tmdb.org/t/p/w185${poster_path}`}
+          alt={`${title} - clique para ver mais`}
+          className="movie-img h-72 rounded-sm"
+        />
+        <div className="movie-info top-[0] p-2">
+          <h2 id={`movie-title-${id}`} className="text-base">
+            {title}
+          </h2>
+          <p className="text-xs text-gray-600">clique para ver detalhes</p>
         </div>
+      </Link>
+      <div className="movie-info bottom-[2%] px-2">
+        <CustomTooltip
+          placement="top"
+          arrow
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 300 }}
+          title={
+            isFavorite ? "Remover dos Favoritos" : "Adicionar aos Favoritos"
+          }
+        >
+          <button
+            onClick={toggleFavorite}
+            className="pr-1"
+            aria-pressed={isFavorite} // ARIA attribute to indicate toggle state
+            aria-label={
+              isFavorite ? "Remover dos Favoritos" : "Adicionar aos Favoritos"
+            } // ARIA label for screen readers
+          >
+            {isFavorite ? (
+              <FavoriteIcon className="icon text-[red]" />
+            ) : (
+              <FavoriteBorderIcon className="icon text-white" />
+            )}
+          </button>
+        </CustomTooltip>
+        <CustomTooltip
+          placement="top"
+          arrow
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 300 }}
+          title={
+            isAdd
+              ? "Remover da Lista de interesse"
+              : "Adicionar a Lista de interesse"
+          }
+        >
+          <button
+            onClick={toggleAdd}
+            aria-pressed={isAdd} // ARIA attribute to indicate toggle state
+            aria-label={
+              isAdd
+                ? "Remover da Lista de interesse"
+                : "Adicionar a Lista de interesse"
+            } // ARIA label for screen readers
+          >
+            {isAdd ? (
+              <AddCircleIcon className="icon" />
+            ) : (
+              <AddCircleOutlineIcon className="icon" />
+            )}
+          </button>
+        </CustomTooltip>
       </div>
-    </>
+    </div>
   );
 }
