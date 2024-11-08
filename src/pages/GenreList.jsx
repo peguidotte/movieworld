5b74bb3388248ec72a2ca72a45cd7d854873d055
PedroGuidotte <peguidotte@gmail.com>
2024-11-07 21:16:09 -0300

    GenrePage finalizada
    Usuário capaz de visualizar os melhores filmes por gênero.

--- src/pages/GenreList.jsx
+++ src/pages/GenreList.jsx
@@ -1,17 +1,53 @@
-import GenreCard from '../components/GenreCard';
+import React, { useEffect, useState } from 'react';
+import MoviesByGenre from './MoviesByGenrePage';
+import 'ldrs/bouncy';
 
-export default function GenreList(){
-    return(
+export default function GenreList() {
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-BR`
+      );
+      const data = await response.json();
+      setGenres(data.genres);
+      setLoading(false);
+    };
+
+    fetchGenres();
+  }, []);
+
+  const selectedGenreName = genres.find(genre => genre.id === selectedGenre)?.name;
+
+  return (
+    <div className='m-3'>
+      {loading ? (
+        <div className='flex justify-center items-center h-64'>
+          <l-bouncy size="45" speed="1.75" color="white"></l-bouncy>
+        </div>
+      ) : (
         <>
-         <h2 className='col-span-4 text-2xl font-bold text-center mb-5'>Veja os filmes por gênero</h2>
-         <main className='flex flex-wrap gap-10 justify-center'>
-        {
-            data.map((genre) => (
-                    <GenreCard key={genre.id} {...genre}/>
-                )
-            )
-        }
-        </main>
+          <main className='flex flex-wrap gap-5 justify-center'>
+            {genres.map((genre) => (
+              <button
+                key={genre.id}
+                className={`p-2 rounded ${selectedGenre === genre.id ? 'bg-purple-600 text-white' : 'bg-purple-800 text-white'}`}
+                onClick={() => setSelectedGenre(genre.id)}
+              >
+                {genre.name}
+              </button>
+            ))}
+          </main>
+          {selectedGenre ? (
+            <MoviesByGenre genreId={selectedGenre} genreName={selectedGenreName} />
+          ) : (
+            <p className='text-center mt-10'>Escolha um gênero para te exibirmos os melhores!</p>
+          )}
         </>
-    )
+      )}
+    </div>
+  );
 }
